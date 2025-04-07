3679b2befe912f9db8e0d027e8e9a80c08a2aad4
vladpavlov <vlad.pavlov@namecheap.com>
2025-04-07 21:36:41 +0200

    add html reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
     testMatch: ['**/tests/**/*.ts'],
+    reporters: [
+        "default",
+        [ "jest-html-reporter", {
+            pageTitle: "Test Report",
+            outputPath: "./reports/test-report.html"
+        }]
+    ]
 };
